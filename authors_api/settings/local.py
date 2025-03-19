0858e278fedb7ae45846be3305feef81e62ffffe
mostafam69 <mo.h@cfbtel.com>
2025-03-19 09:49:03 +0330

    create project

---
+++ authors_api/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *  # noqa
+from .base import env
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="lnfsPmXP00F7iH5P_6xbfdcwGSAWAARMvIz9B4G6Z96Z15CY4Ic",
+    )
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
